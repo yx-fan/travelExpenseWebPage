97b4e359835787c0f61a1ce10ff1e0dc7a6e04ea
yx-fan <48743988+yx-fan@users.noreply.github.com>
2024-06-22 19:20:16 -0400

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,8 @@
 # ng build --configuration production --base-href "https://yx-fan.github.io/travelExpenseWebPage/" // When deploying to GitHub Pages using a specific project name
 ng build --configuration production --base-href "/"
 
+# Copy CNAME file to dist folder
+cp CNAME dist/travel-expense-webpage/CNAME
+
 # Deploy Angular app to GitHub Pages
 npx angular-cli-ghpages --dir=dist/travel-expense-webpage
