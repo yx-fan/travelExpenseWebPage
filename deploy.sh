bd189d8ba09f20a1d93e1d6c65b89c3dbbe55662
yx-fan <48743988+yx-fan@users.noreply.github.com>
2024-06-22 19:12:14 -0400

    update deploy codes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Build Angular app
-ng build --configuration production --base-href "https://yx-fan.github.io/travelExpenseWebPage/"
+# ng build --configuration production --base-href "https://yx-fan.github.io/travelExpenseWebPage/" // When deploying to GitHub Pages using a specific project name
+ng build --configuration production --base-href "/"
 
 # Deploy Angular app to GitHub Pages
 npx angular-cli-ghpages --dir=dist/travel-expense-webpage
